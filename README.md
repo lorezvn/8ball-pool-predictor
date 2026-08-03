f4e8b94f7690489e80cb76ac82194627f27ee9ec
Lorenzo Zanda <ezan.computerscience@gmail.com>
2026-08-03 18:47:27 +0200

    update README.md, minor fixes

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Project developed for the Computer Vision Course 2025/2026
 - Lorenzo Zanda (2006432)
 - Davide Vittucci (1903954)
 - Paolo Marchetti (1986485)
+
+## TO-DO 
+- [ ] Ottimizzare processi di merging datasets
+- [ ] Detect palle, stecca, direzione
+- [ ] Mappa 2D
+- [ ] Sistemare struttura del progetto
+- [ ] Dimostrazione visiva
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     create_folders()
     set_seed(SEED)
     build_merged_dataset(MERGED_DIR, valid_video_ids={"video-3"}, dry_run=False)
-    # for dataset in (V2, V3, MERGED_DIR): check_dataset(dataset)
+    #for dataset in (V2, V3, MERGED_DIR): check_dataset(dataset)
     qa_sample(dataset_path=MERGED_DIR, split="train", n_samples=10, delete=True)
 
 if __name__ == '__main__':
